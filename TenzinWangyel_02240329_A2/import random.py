f2b405c5beb942c146c60863524c7777a79d2351
tenxin54 <02240329.cst@rub.edu.bt>
2025-05-09 13:10:03 +0200

    Practical Assignment 2

--- TenzinWangyel_02240329_A2/import random.py
+++
@@ -1,6 +0,0 @@
-import random
-class GuessNumberGame:
-    def __init__(self, lower=1, higher=10)
-        self.lower=lower
-        self.higher=higher
-        self.secretnumber=random.randint(lower, higher)
